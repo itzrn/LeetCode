8c3854d57e66bed96f8ec315b8fdd90b32be78a7
Aryan Prajapati <aryangithub2017@gmail.com>
2023-09-03 13:16:48 +0530

    L62
    update

--- src/L62_UniquePath.java
+++ src/L62_UniquePath.java
@@ -29,6 +29,63 @@ public class L62_UniquePath {
 
         return dp[m-1][n-1];
     }
+
+    // this gives TLE in LeetCode
+    // this is not efficient bcz of the overlapping recursive calls
+    public int uniquePaths2(int m, int n) { // this is proper dfs, here we are not working on any grid, here we are working on numbers oonly
+        if(m == 1 && n ==1){
+            return 1;
+        }
+        return 1 + count(m,n);
+    }
+
+    public int count(int m, int n){
+        if(m == 1 && n ==1){
+            return 1;
+        }
+
+        if((m == 1 && n>1) || n == 1 && m > 1){
+            return 0;
+        }
+
+        return  1 + count(m-1,n) + count(m,n-1);
+    }
+
+    // best solution
+    public int uniquePaths3(int m, int n) {
+        int[][] dp = new int[m+1][n+1]; // making the dp array size of (m+1, n+1), bcz we have to calculate the number of unique path for (m,n) as index
+        return recursiveDp(dp, m, n);
+    }
+    /*
+    let m=3,n=7
+    dp ->
+        0  0  0  0  0  0   0   0 -> this line is doing padding, so not to go out of bound, and even the left most column is also doing the same
+        0  1  1  1  1  1   1   1
+        0  1  2  3  4  5   6   7
+        0  1  3  6 10  15  21  28
+
+        intuition ->
+        we will start from 3,7 and do dfs
+        while doing back track we will calculate the unique path to reach from 1,1 to m.n and store that number of unique path at m,n
+        this storing of unique path help to not go in already went recursion
+    */
+
+    public int recursiveDp(int[][] dp, int m, int n){
+        if(m == 0 || n == 0){ // this case only works when m=0, n=0
+            return 0;
+        }
+
+        if(m == 1 && n == 1){ // base case
+            return 1;
+        }
+
+        if(dp[m][n]>0){ // when this condition is true means, we need not go in the particular recursion else return the value of dp[m][n]
+            return dp[m][n];
+        }
+
+        dp[m][n] = recursiveDp(dp,m-1,n) + recursiveDp(dp,m,n-1); // calculating the number of unique path, when starting from 1,1 to m,n
+        return dp[m][n]; // returning the unique path for m,n staring from 1,1
+    }
 }
 
 /*
