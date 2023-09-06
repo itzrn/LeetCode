0ae2f99a11883cde0f55d0c73492cdfee595b233
Aryan Prajapati <aryangithub2017@gmail.com>
2023-09-06 18:04:57 +0530

    L70
    Leetcode

--- src/L70_ClimbingStairs.java
+++ src/L70_ClimbingStairs.java
@@ -1,54 +1,57 @@
 package src;
 
-/*
-You are climbing a staircase. It takes n steps to reach the top.
-Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
-Example 1:
-
-Input: n = 2
-Output: 2
-Explanation: There are two ways to climb to the top.
-1. 1 step + 1 step
-2. 2 steps
-
-
-Example 2:
-
-Input: n = 3
-Output: 3
-Explanation: There are three ways to climb to the top.
-1. 1 step + 1 step + 1 step
-2. 1 step + 2 steps
-3. 2 steps + 1 step
-
-
-
-test cases ---> 1, 45, 2, 3
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 45
  */
 
 public class L70_ClimbingStairs {
     public int climbStairs(int n) {
-        // the commented code is recursion way, but it extends the time alot to calculate
-        // if(n == 1){
-        //     return 1;
-        // }
-        // if(n == 2){
-        //     return 2;
-        // }
+        int[] dp = new int[n];
+        return recursiveDp(dp,n-1);
+    }
 
-        // return climbStairs(n-1) + climbStairs(n - 2);
-
-        int[] array = new int[n];
-        array[0] = 1;
-        if(n > 1){
-            array[1] = 2;
+    public int recursiveDp(int[] dp, int n){ // using top-down dp, means instead of climbing up the stairs, climbing down the stairs
+        if(n < -1){ // base case
+            return 0;
+        }
+        if(n == -1){ // if n becomes -1 means we reached at the top of the stairs
+            return 1;
+        }
+        if(dp[n] > 0){ // dp is used to store the number of ways to reach at the top from stair n, so if in future we reach at this point, already visited point
+            // we won't go in any further recursion, instead we calculate it in O(1)
+            return dp[n];
         }
 
-        for(int i = 2; i<n;i++){
-            array[i] = array[i-1] + array[i-2];
-        }
+        int oneStep = recursiveDp(dp, n-1); // moving one step
+        int twoStep = recursiveDp(dp, n-2); // moving two steps
 
-        return array[n-1];
+        dp[n] = oneStep + twoStep; // storing ways to reach at the top from stair n
+
+        return dp[n]; // returning dp[n], bcz we want the ways to reach at the top when we ar the starting stairs
     }
 }
